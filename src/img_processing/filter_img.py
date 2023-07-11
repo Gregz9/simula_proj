33e258fc8f02a2b0660b3aade51c12ae6625ca3a
Gregz9 <gregorkajda@gmail.com>
2023-07-11 13:47:16 +0200

    Script for processing images
    Signed-off-by: Gregz9 <gregorkajda@gmail.com>

--- src/img_processing/filter_img.py
+++ src/img_processing/filter_img.py
@@ -3,29 +3,84 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 import os
+from math import atan2, cos, sin, sqrt, pi
 
 def file_size(path):
     file_size = os.path.getsize(path)
     # file size in MB
     return round(file_size/1024**2, 2)
-#
-# ap = argparse.ArgumentParser()
-# args = vars(ap.parse_args())
-#
-#
+
+def drawAxis(img, p_, q_, color, scale):
+  p = list(p_)
+  q = list(q_)
+ 
+  ## [visualization1]
+  angle = atan2(p[1] - q[1], p[0] - q[0]) # angle in radians
+  hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
+ 
+  # Here we lengthen the arrow by a factor of scale
+  q[0] = p[0] - scale * hypotenuse * cos(angle)
+  q[1] = p[1] - scale * hypotenuse * sin(angle)
+  cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
+ 
+  # create the arrow hooks
+  p[0] = q[0] + 9 * cos(angle + pi / 4)
+  p[1] = q[1] + 9 * sin(angle + pi / 4)
+  cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
+ 
+  p[0] = q[0] + 9 * cos(angle - pi / 4)
+  p[1] = q[1] + 9 * sin(angle - pi / 4)
+  cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
+  ## [visualization1]
+ 
+def getOrientation(pts, img):
+  ## [pca]
+  # Construct a buffer used by the pca analysis
+  sz = len(pts)
+  data_pts = np.empty((sz, 2), dtype=np.float64)
+  for i in range(data_pts.shape[0]):
+    data_pts[i,0] = pts[i,0,0]
+    data_pts[i,1] = pts[i,0,1]
+ 
+  # Perform PCA analysis
+  mean = np.empty((0))
+  mean, eigenvectors, eigenvalues = cv.PCACompute2(data_pts, mean)
+ 
+  # Store the center of the object
+  cntr = (int(mean[0,0]), int(mean[0,1]))
+  ## [pca]
+ 
+  ## [visualization]
+  # Draw the principal components
+  cv.circle(img, cntr, 3, (255, 0, 255), 2)
+  p1 = (cntr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0,0], cntr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0,0])
+  p2 = (cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0], cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0])
+  drawAxis(img, cntr, p1, (255, 255, 0), 1)
+  drawAxis(img, cntr, p2, (0, 0, 255), 5)
+ 
+  angle = atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
+  ## [visualization]
+ 
+  # Label with the rotation angle
+  label = "  Rotation Angle: " + str(-int(np.rad2deg(angle)) - 90) + " degrees"
+  textbox = cv.rectangle(img, (cntr[0], cntr[1]-25), (cntr[0] + 250, cntr[1] + 10), (255,255,255), -1)
+  cv.putText(img, label, (cntr[0], cntr[1]), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv.LINE_AA)
+ 
+  return angle
+
+
 # image = cv.imread(args["image"])
 # print("Image compressed successfully")
 # print(file_size("compressed_image.jpg"))
 
-
-path = "/home/gregz/Files/simula_proj/src/img_processing/IMG_8613.jpg"
+path = "/home/gregz/Files/simula_proj/src/img_processing/IMG_8615.jpg"
 
 # print(file_size(path))
 ap = argparse.ArgumentParser()
 ap.add_argument("-r", "--resizefactor", required=True, help = "Downscaling factor, values 0.0 to 1.0")
 ap.add_argument("-c","--compression", required=True, help = "Compression level")
 ap.add_argument("-ks","--kernelsize", required=False, help = "Kernel size for Gaussian blur filtering")
-ap.add_argument("-sf","--sobelfilter", required=False, help = "If yes, the processed image will be filtered with sobel kernels")
+ap.add_argument("-sf","--sobelfilter", required=False, action="store_true", help = "If yes, the processed image will be filtered with sobel kernels")
 args = vars(ap.parse_args())
 # ap.add_argument("-i","--image", required = True, help = "Path to input file")
 try: 
@@ -56,10 +111,11 @@ cv.imwrite("compressed_image.jpg", resized, [cv.IMWRITE_JPEG_QUALITY, compressio
 
 comp_img = cv.imread("compressed_image.jpg")
 filter_img = cv.GaussianBlur(comp_img, (kernel_size, kernel_size),cv.BORDER_DEFAULT)
-# # filter_img = cv.GaussianBlur(filter_img, (13, 13),cv.BORDER_DEFAULT)
-#
+
+cv.imwrite("processed_image.jpg", filter_img)
+
 # hsv = cv.cvtColor(filter_img, cv.COLOR_BGR2HSV)
-if str(args["sobelfilter"]) == "y": 
+if args["sobelfilter"]: 
     sobelx = cv.Sobel(filter_img, cv.CV_64F,1,0,ksize=5)
     sobely = cv.Sobel(filter_img, cv.CV_64F,0,1,ksize=5)
     plt.imshow(sobelx)
@@ -67,6 +123,21 @@ if str(args["sobelfilter"]) == "y":
 else:
     plt.imshow(filter_img)
     plt.show()
-#
-# # cv.imshow("",sobelx)
-#
+
+gray_filter_img = cv.cvtColor(filter_img, cv.COLOR_BGR2GRAY)
+_, bw = cv.threshold(gray_filter_img, 50, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+contours, _ = cv.findContours(bw, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+
+for i, c in enumerate(contours): 
+    area = cv.contourArea(c)
+
+    if area < 3700 or 100000 < area: 
+        continue 
+
+    cv.drawContours(filter_img, contours, i, (0,0,255), 2)
+
+    getOrientation(c, filter_img)
+
+cv.imshow('Output image', filter_img)
+cv.waitKey(0)
+cv.destroyAllWindows()
